65919044c8e56335fa50eb73cc76438f83a87bc1
3jisung <3jisung@naver.com>
2023-05-19 10:04:49 +0900

    test

---
+++ Homework/Homework_0518/Homework_0518/Homework_0518.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 배열의 글자 수를 세는 함수
+int StringCount(char* _Arr)
+{
+    if (_Arr == nullptr)
+    {
+        cout << "nullptr가 참조 되었습니다." << endl;
+        return -1;
+    }
+
+    int cnt = -1;
+    while (_Arr[++cnt]);
+
+    return cnt;
+}
+
+// 배열 내 글자 내용을 변경하는 함수
+void ChangeCh(char* _Arr, char _PrevCh, char _NextCh)
+{
+    if (_Arr == nullptr)
+    {
+        cout << "nullptr가 참조 되었습니다." << endl;
+        return;
+    }
+
+    int idx = 0;
+    while (_Arr[idx++])
+    {
+        if (_Arr[idx] == _PrevCh)
+        {
+            _Arr[idx] = _NextCh;
+        }
+    }
+
+    return;
+}
+
+int main()
+{
+    // 한글 들어가지 않습니다.
+    {
+        char Arr[100] = "aa0aa";
+        int Result = StringCount(Arr);
+
+        if (Result != -1)
+        {
+            cout << "StringCount Method" << endl << Arr << " 의 글자 수 : " << Result << endl;
+        }
+    }
+
+    {
+        char Arr[100] = "aaa bbb ccc";
+        cout << endl << endl << "ChangeCh Method" << endl << "변경 전 : " << Arr << endl;
+
+        ChangeCh(Arr, 'c', 'd');
+
+        cout << "변경 후 : " << Arr << endl;
+    }
+}
